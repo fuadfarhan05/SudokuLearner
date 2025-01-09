55f0f47c8ef7073a5820be68850d73e96ff95d00
Fuad Farhan <fuadhoquef8414@hstat.org>
2025-01-09 17:19:40 -0500

    layout

--- solve.py
+++ solve.py
@@ -1,33 +1,39 @@
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [7, 8, " ", 4, " ", " ", 1, 2, " "],
+    [6, " ", " ", " ", 7, 5, " ", " ", 9],
+    [" ", " ", " ", 6, " ", 1, " ", 7, 8],
+    [" ", " ", 7, " ", 4, " ", 2, 6, " "],
+    [" ", " ", 1, " ", 5, " ", 9, 3, " "],
+    [9, " ", 4, " ", 6, " ", " ", " ", 5],
+    [" ", 7, " ", 3, " ", " ", " ", 1, 2],
+    [1, 2, " ", " ", " ", 7, 4, " ", " "],
+    [" ", 4, 9, 2, " ", 6, " ", " ", 7]
 ]
 
+''' [try more boards below ]
+[5, 3, " ", " ", 7, " ", " ", " ", " "],
+    [6, " ", " ", 1, 9, 5, " ", " ", " "],
+    [" ", 9, 8, " ", " ", " ", " ", 6, " "],
+    [8, " ", " ", " ", 6, " ", " ", " ", 3],
+    [4, " ", " ", 8, " ", 3, " ", " ", 1],
+    [7, " ", " ", " ", 2, " ", " ", " ", 6],
+    [" ", 6, " ", " ", " ", " ", 2, 8, " "],
+    [" ", " ", " ", 4, 1, 9, " ", " ", 5],
+    [" ", " ", " ", " ", 8, " ", " ", 7, 9]
 
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
+     [8, " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", 3, 6, " ", " ", " ", " ", " "],
+    [" ", 7, " ", " ", 9, " ", 2, " ", " "],
+    [" ", 5, " ", " ", " ", 7, " ", " ", " "],
+    [" ", " ", " ", " ", 4, 5, 7, " ", " "],
+    [" ", " ", " ", 1, " ", " ", " ", 3, " "],
+    [" ", " ", 1, " ", " ", " ", " ", 6, 8],
+    [" ", " ", 8, 5, " ", " ", " ", 1, " "],
+    [" ", 9, " ", " ", " ", " ", 4, " ", " "]
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
 
-            if solve(bo):
-                return True
 
-            bo[row][col] = 0
-
-    return False
+    '''
 
 
 def valid(bo, num, pos):
@@ -45,9 +51,9 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
                 return False
 
     return True
@@ -71,10 +77,55 @@ def print_board(bo):
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+            if bo[i][j] == " ":
                 return (i, j)  # row, col
 
     return None
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = " "
+
+    return False
+
+
+
+
+
 
 print("_______Sudoku Board_______")
 print_board(board)
